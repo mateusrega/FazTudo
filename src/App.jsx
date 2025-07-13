869c6f3b5ee9bd10ca1410e01c4b9d863b604cea
mateusrega <143010456+mateusrega@users.noreply.github.com>
2025-07-13 20:12:02 -0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import Builder from "./pages/Builder";
+import Feedback from "./pages/Feedback";
+import { UserProvider } from "./contexts/UserContext";
+
+function App() {
+  return (
+    <UserProvider>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/builder/:id" element={<Builder />} />
+          <Route path="/feedback" element={<Feedback />} />
+          <Route path="*" element={<Navigate to="/login" />} />
+        </Routes>
+      </Router>
+    </UserProvider>
+  );
+}
+
+export default App;
