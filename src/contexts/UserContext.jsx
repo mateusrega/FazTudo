5cc2fe74d311172904caac2049a2b21037901b3a
mateusrega <143010456+mateusrega@users.noreply.github.com>
2025-07-13 19:46:58 -0300

    Create UserContext.jsx

---
+++ src/contexts/UserContext.jsx
@@ -0,0 +1,24 @@
+import React, { createContext, useState, useEffect } from "react";
+import { auth } from "../services/firebase";
+import { onAuthStateChanged } from "firebase/auth";
+
+export const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <UserContext.Provider value={{ user, loading }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
