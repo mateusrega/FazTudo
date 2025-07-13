f1da9e7002f7498c60fbefa315d285f5a0d73ace
mateusrega <143010456+mateusrega@users.noreply.github.com>
2025-07-13 20:07:51 -0300

    Create Builder.jsx

---
+++ src/pages/Builder.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Builder = () => {
+  return (
+    <div className="p-6 max-w-xl mx-auto">
+      <h1 className="text-2xl mb-4">Editor de Automações</h1>
+      <p>Em breve você poderá criar automações arrastando blocos aqui...</p>
+    </div>
+  );
+};
+
+export default Builder;
