60ecf340eda40efa62d56bddefcfc189e817eb58
mateusrega <143010456+mateusrega@users.noreply.github.com>
2025-07-15 02:28:53 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,31 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: "FazTudo - Automação Pessoal",
+        short_name: "FazTudo",
+        theme_color: "#0d47a1",
+        background_color: "#0d47a1",
+        display: "standalone",
+        start_url: ".",
+        icons: [
+          {
+            src: "logo-192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "logo-512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      }
+    })
+  ]
 });
