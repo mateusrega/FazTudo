4ef3b08ab07dc793c63d7ed024aaa92fdfbab226
mateusrega <143010456+mateusrega@users.noreply.github.com>
2025-07-18 23:40:07 -0300

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,10 +3,11 @@ import React from "react";
 import Sidebar from "../components/Sidebar";
 
 const modoTeste = localStorage.getItem("modoTeste") === "true";
+
 const Dashboard = () => {
   return (
     <div className="flex">
-      <Sidebar />
+      <Sidebar modoTeste={modoTeste} />
       <main className="ml-60 p-6 w-full">
         <h1 className="text-3xl font-bold mb-6">⚙️ Minhas Automações</h1>
         <p className="text-gray-600">Você ainda não criou nenhuma automação.</p>
