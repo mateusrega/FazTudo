bb51d97f542c352a5f96a2478aa84891c8e434a5
mateusrega <143010456+mateusrega@users.noreply.github.com>
2025-07-19 01:20:56 -0300

    Create toolbox.js

---
+++ blockly/toolbox.js
@@ -0,0 +1,13 @@
+export const toolboxXml = `
+<xml xmlns="https://developers.google.com/blockly/xml">
+  <category name="Gatilhos" colour="#FFBF00">
+    <block type="webhook_trigger"></block>
+  </category>
+  <category name="Ações" colour="#5BA58C">
+    <block type="whatsapp_message"></block>
+    <block type="save_google_sheets"></block>
+    <block type="telegram_notification"></block>
+    <block type="auto_reply"></block>
+  </category>
+</xml>
+`;
