430ae39bf48de0bc87c7564032f17469c34a9faf
mateusrega <143010456+mateusrega@users.noreply.github.com>
2025-07-19 03:10:40 -0300

    Create  customBlocks.js

---
+++ src/blockly/customBlocks.js
@@ -0,0 +1,41 @@
+Blockly.defineBlocksWithJsonArray([
+  {
+    type: "webhook_trigger",
+    message0: "Quando receber Webhook",
+    nextStatement: null,
+    style: "gatilhos",
+    tooltip: "Dispara ao receber um Webhook"
+  },
+  {
+    type: "whatsapp_message",
+    message0: "Enviar mensagem no WhatsApp",
+    previousStatement: null,
+    nextStatement: null,
+    style: "acoes",
+    tooltip: "Envia uma mensagem"
+  },
+  {
+    type: "save_google_sheets",
+    message0: "Salvar no Google Sheets",
+    previousStatement: null,
+    nextStatement: null,
+    style: "acoes",
+    tooltip: "Salva dados no Sheets"
+  },
+  {
+    type: "telegram_notification",
+    message0: "Notificar no Telegram",
+    previousStatement: null,
+    nextStatement: null,
+    style: "acoes",
+    tooltip: "Envia no Telegram"
+  },
+  {
+    type: "auto_reply",
+    message0: "Enviar resposta automática",
+    previousStatement: null,
+    nextStatement: null,
+    style: "acoes",
+    tooltip: "Responde automaticamente"
+  }
+]);
