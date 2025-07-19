c755083b105f1a55d56fe3b6edeec3f1957b52de
mateusrega <143010456+mateusrega@users.noreply.github.com>
2025-07-19 03:11:31 -0300

    Create index.js

---
+++ src/blockly/index.js
@@ -0,0 +1,23 @@
+import Blockly from "blockly";
+import { toolboxXml } from "./toolbox";
+import "./customBlocks";
+import { fazTudoTheme } from "./theme";
+
+export function initBlockly(workspaceRef) {
+  const workspace = Blockly.inject(workspaceRef, {
+    toolbox: toolboxXml,
+    theme: fazTudoTheme,
+    scrollbars: true,
+    trashcan: true,
+    zoom: {
+      controls: true,
+      wheel: true,
+      startScale: 1,
+      maxScale: 3,
+      minScale: 0.3,
+      scaleSpeed: 1.2
+    }
+  });
+
+  return workspace;
+}
