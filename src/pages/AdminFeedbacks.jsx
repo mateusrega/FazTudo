d4bf70789f8020c4ed6f7558e1e1d0163eccab12
mateusrega <143010456+mateusrega@users.noreply.github.com>
2025-07-31 10:57:43 -0300

    Update AdminFeedbacks.jsx

--- src/pages/AdminFeedbacks.jsx
+++ src/pages/AdminFeedbacks.jsx
@@ -396,9 +396,10 @@ const AdminFeedbacks = () => {
                                 </span>
                               )}
                             </div>
-                            <p className="text-gray-900 mb-3 whitespace-pre-line leading-relaxed">
-                            {mensagem}
-                            </p>
+                           <p className="text-gray-900 mb-3 whitespace-pre-wrap break-words leading-relaxed max-w-full">
+  {mensagem}
+</p>
+
 
                             <div className="flex items-center gap-2 text-xs text-gray-500">
                               <FaUsers className="text-gray-400" />
