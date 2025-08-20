47e1dd1e084e4c2aa6f5b548d57441db4502998b
mateusrega <143010456+mateusrega@users.noreply.github.com>
2025-08-20 09:11:05 -0300

    Update Builder.jsx

--- src/pages/Builder.jsx
+++ src/pages/Builder.jsx
@@ -51,7 +51,7 @@ export default function Builder() {
                 <li>• <strong>Lógica:</strong> Adicione condições e decisões</li>
                 <li>• <strong>Conecte os blocos:</strong> Arraste e solte para criar o fluxo</li>
               </ul>
-              <a href: "src/components/animation.jsx">animação</a>
+              <strong><a href: "animacao.html">animação</a></strong>
             </div>
           </div>
         </div>
