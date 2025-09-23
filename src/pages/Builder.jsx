1f64c74ed8c19740abee69ddf588c0f6dbea8992
mateusrega <143010456+mateusrega@users.noreply.github.com>
2025-09-23 11:33:38 -0300

    Update Builder.jsx

--- src/pages/Builder.jsx
+++ src/pages/Builder.jsx
@@ -1,6 +1,4 @@
-// src/components/Builder.jsx
-import React, { useEffect, useRef } from "react";
-import * as Blockly from "blockly/core";// src/pages/Builder.jsx
+// src/pages/Builder.jsx
 import React, { useEffect, useRef, useState } from "react";
 import Sidebar from "../components/Sidebar";
 import VoiceRecorder from "../components/VoiceRecorder";
@@ -23,17 +21,10 @@ export default function Builder() {
       trashcan: true,
       horizontalLayout: false,
       zoom: { controls: true, wheel: true, startScale: 1, maxScale: 2, minScale: 0.3 },
-      move: {
-        scrollbars: true,
-        drag: true,
-        wheel: false,
-        dragOptions: {
-          dragStartHysteresis: 0, // arraste instantâneo
-        },
-      },
+      move: { scrollbars: true, drag: true, wheel: false },
     });
 
-    // Ajusta a lixeira: menor e mais para cima
+    // Ajusta a lixeira
     const trash = workspaceRef.current.trashcan_;
     if (trash && trash.svgGroup_) {
       trash.svgGroup_.setAttribute("transform", "translate(0,-40) scale(0.33)");
@@ -51,7 +42,6 @@ export default function Builder() {
     };
   }, []);
 
-  // Alternar fullscreen
   const toggleFullscreen = () => {
     const container = document.getElementById("builder-container");
     if (!container) return;
@@ -68,7 +58,6 @@ export default function Builder() {
       setIsFullscreen(false);
     }
 
-    // Recalcula o tamanho do Blockly
     setTimeout(() => {
       if (workspaceRef.current) Blockly.svgResize(workspaceRef.current);
     }, 300);
@@ -91,7 +80,7 @@ export default function Builder() {
           <FaLightbulb className="text-yellow-500 text-xl" />
         </div>
 
-        {/* Área do editor */}
+        {/* Editor */}
         <div className="flex-1 p-2">
           <div
             ref={blocklyDiv}
@@ -108,47 +97,36 @@ export default function Builder() {
               <block type="controls_if"></block>
               <block type="controls_repeat_ext"></block>
             </category>
-
             <category name="Ações" colour="#4CAF50">
               <block type="text_print"></block>
               <block type="text"></block>
             </category>
-
             <category name="Lógica" colour="#3F51B5">
               <block type="logic_compare"></block>
               <block type="logic_operation"></block>
               <block type="logic_boolean"></block>
             </category>
-
             <category name="Números" colour="#795548">
               <block type="math_number"></block>
               <block type="math_arithmetic"></block>
             </category>
-
             <category name="Texto" colour="#9C27B0">
               <block type="text"></block>
               <block type="text_join"></block>
               <block type="text_length"></block>
             </category>
-
             <category name="Cor" colour="#A6745C">
               <block type="colour_picker"></block>
               <block type="colour_random"></block>
               <block type="colour_rgb">
                 <value name="RED">
-                  <shadow type="math_number">
-                    <field name="NUM">100</field>
-                  </shadow>
+                  <shadow type="math_number"><field name="NUM">100</field></shadow>
                 </value>
                 <value name="GREEN">
-                  <shadow type="math_number">
-                    <field name="NUM">50</field>
-                  </shadow>
+                  <shadow type="math_number"><field name="NUM">50</field></shadow>
                 </value>
                 <value name="BLUE">
-                  <shadow type="math_number">
-                    <field name="NUM">0</field>
-                  </shadow>
+                  <shadow type="math_number"><field name="NUM">0</field></shadow>
                 </value>
               </block>
             </category>
@@ -160,103 +138,3 @@ export default function Builder() {
     </div>
   );
 }
-
-import "blockly/blocks"; // blocos padrões
-import "blockly/javascript"; // gerador JS
-
-const Builder = () => {
-  const blocklyDiv = useRef(null);
-  const toolbox = useRef(null);
-  const workspace = useRef(null);
-
-  useEffect(() => {
-    if (!blocklyDiv.current || !toolbox.current) return;
-
-    // Inicializa Blockly
-    workspace.current = Blockly.inject(blocklyDiv.current, {
-      toolbox: toolbox.current,
-      trashcan: true,
-      scrollbars: true,
-      renderer: "zelos",
-      move: {
-        scrollbars: true,
-        drag: true,
-        wheel: true,
-      },
-    });
-
-    return () => {
-      workspace.current?.dispose();
-    };
-  }, []);
-
-  return (
-    <div className="w-full h-screen flex flex-col bg-gray-100">
-      {/* Área do editor */}
-      <div className="flex-1 p-2">
-        <div
-          ref={blocklyDiv}
-          className="w-full h-full border rounded-xl bg-white shadow-md"
-        />
-
-        {/* Toolbox com blocos simples */}
-        <xml
-          xmlns="https://developers.google.com/blockly/xml"
-          style={{ display: "none" }}
-          ref={toolbox}
-        >
-          <category name="Eventos" colour="#FFAB19">
-            <block type="controls_if"></block>
-            <block type="controls_repeat_ext"></block>
-          </category>
-
-          <category name="Ações" colour="#4CAF50">
-            <block type="text_print"></block>
-            <block type="text"></block>
-          </category>
-
-          <category name="Lógica" colour="#3F51B5">
-            <block type="logic_compare"></block>
-            <block type="logic_operation"></block>
-            <block type="logic_boolean"></block>
-          </category>
-
-          <category name="Números" colour="#795548">
-            <block type="math_number"></block>
-            <block type="math_arithmetic"></block>
-          </category>
-
-          <category name="Texto" colour="#9C27B0">
-            <block type="text"></block>
-            <block type="text_join"></block>
-            <block type="text_length"></block>
-          </category>
-
-          <category name="Cor" colour="#A6745C">
-            <block type="colour_picker"></block>
-            <block type="colour_random"></block>
-            <block type="colour_rgb">
-              <value name="RED">
-                <shadow type="math_number">
-                  <field name="NUM">100</field>
-                </shadow>
-              </value>
-              <value name="GREEN">
-                <shadow type="math_number">
-                  <field name="NUM">50</field>
-                </shadow>
-              </value>
-              <value name="BLUE">
-                <shadow type="math_number">
-                  <field name="NUM">0</field>
-                </shadow>
-              </value>
-            </block>
-          </category>
-        </xml>
-      </div>
-    </div>
-  );
-};
-
-export default Builder;
